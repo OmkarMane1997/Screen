dd8414c4ca7ede4fc10f879f240682adc9e70e41
omkarMane1997 <omii0.mane@gmail.com>
2023-01-17 14:27:01 +0530

    Footer add

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,19 +1,32 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Divider, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import '../assets/User.css'
 
 function Footer() {
+
+  
   return (
-    <Box>
-      <Typography
-        level="body2"
-        startDecorator={
-          <Typography textColor="text.tertiary">2022 ©</Typography>
-        }
-      >
-        KeenThemes
-      </Typography>
-    </Box>
+    <>
+      <Divider />
+      <Grid container spacing={2}>
+        <Grid item lg={9} md={8} sm={6} xs={12} sx={{ my: 2 }}>
+          <Box>
+            <Typography level="body2">2022 © KeenThemes</Typography>
+          </Box>
+        </Grid>
+        <Grid item lg={3} md={4} sm={6} xs={12} sx={{ my: 2 }}>
+          <Stack direction="row" spacing={2}>
+            <NavLink to={"/"} className='navLink'>About</NavLink>
+            <NavLink to={"/"} className='navLink'>Support</NavLink>
+            <NavLink to={"/"} className='navLink'>Purchase</NavLink>
+          </Stack>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -3,47 +3,62 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { Container, Breadcrumbs, Button, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import "../assets/User.css";
-
+import Grid from "@mui/material/Grid";
+import Footer from '../component/Footer';
 function User() {
   return (
-    <Container fixed>
-      <div>
-        <div className="left-div">
-          <Box component="main" sx={{ flexGrow: 1, pt: 9 }}>
-            <Typography variant="h6" align="left" gutterBottom>
-              <b>Users List</b>
-            </Typography>
-            <Breadcrumbs aria-label="breadcrumb" separator="-">
-              <NavLink to={"/Home"} className={"navLink"}>
-                Home
-              </NavLink>
-
-              <NavLink to={"/"} className={"navLink"}>
-                User Management{" "}
-              </NavLink>
-              <Typography color="text.primary">Users</Typography>
-            </Breadcrumbs>
-          </Box>
-        </div>
-        <div className="right-div">
-          <Box component="main" sx={{ flexGrow: 1, pt: 9 }}>
-            <Stack direction="row">
-              <Button variant="outlined" sx={{ m: 1 }} startIcon={<FilterAltIcon />} className='filter_button'>
-                Delete
-              </Button>
-              <Button variant="contained" sx={{ m: 1 }} className='button_Background'>
-                Create
-              </Button>
-            </Stack>
-          </Box>
-        </div>
-      </div>
-      
-     
-    </Container>
-    
+    <>
+    <Container fixed sx={{ mt: 9 }}>
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item lg={9} md={6} sm={6} xs={12}>
+            <Box component="main">
+              <Typography variant="h6" align="left" gutterBottom>
+                <b>Users List</b>
+              </Typography>
+              <Breadcrumbs aria-label="breadcrumb" separator="-">
+                <NavLink to={"/Home"} className={"navLink"}>
+                  Home
+                </NavLink>
+                <NavLink to={"/"} className={"navLink"}>
+                  User Management{" "}
+                </NavLink>
+                <Typography color="text.primary">Users</Typography>
+              </Breadcrumbs>
+            </Box>
+          </Grid>
+          <Grid item lg={3} md={6} sm={6} xs={12}>
+            <Box component="main">
+              <Stack direction="row">
+                <Button
+                  variant="outlined"
+                  sx={{ m: 1 }}
+                  startIcon={<FilterAltIcon />}
+                  className="filter_button"
+                >
+                  Delete
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{ m: 1 }}
+                  className="button_Background"
+                >
+                  Create
+                </Button>
+              </Stack>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box>
+        <Typography variant="h1" align="center">
+          Omkar
+        </Typography>
+      </Box>
+      <Footer/>
+    </Container></>
   );
 }
 
